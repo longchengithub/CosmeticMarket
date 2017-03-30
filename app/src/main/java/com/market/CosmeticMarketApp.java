541a0eecd80be64529f19a3c961ed81b7b9f34be
pm_cl <pm_cl@163.com>
2017-03-30 23:46:36 +0800

    a test

--- app/src/main/java/com/market/CosmeticMarketApp.java
+++ app/src/main/java/com/market/CosmeticMarketApp.java
@@ -1,6 +1,10 @@
 package com.market;
 
 import android.app.Application;
+import android.content.Context;
+
+import com.market.dao.DaoMaster;
+import com.market.dao.DaoSession;
 
 /**
  * Created by chenlong on 2017/1/5.
@@ -9,6 +13,8 @@ import android.app.Application;
 public class CosmeticMarketApp extends Application
 {
     private static CosmeticMarketApp mInstance;
+    private static DaoMaster daoMaster;
+    private static DaoSession daoSession;
 
     @Override
     public void onCreate()
@@ -20,5 +26,35 @@ public class CosmeticMarketApp extends Application
     public static CosmeticMarketApp newInstance()
     {
         return mInstance;
+    }
+
+    /**
+     * 取得DaoMaster
+     *
+     * @return daoMaster
+     */
+    private static DaoMaster getDaoMaster(Context context)
+    {
+        DaoMaster.OpenHelper helper = new DaoMaster.DevOpenHelper(context, "CarStore.db", null);
+        daoMaster = new DaoMaster(helper.getWritableDatabase());
+        return daoMaster;
+    }
+
+    /**
+     * 取得DaoSession
+     *
+     * @return daoSession
+     */
+    public static DaoSession getDaoSession(Context context)
+    {
+        if (daoSession == null)
+        {
+            if (daoMaster == null)
+            {
+                daoMaster = getDaoMaster(context);
+            }
+            daoSession = daoMaster.newSession();
+        }
+        return daoSession;
     }
 }
